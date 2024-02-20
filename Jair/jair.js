ed8b693846a1c86c5ab44b625db6e8aba5b7e2f6
Hector Jair Velazquez Jimenez <a340389707@my.uvm.edu.mx>
2024-02-20 17:30:25 -0600

    a

--- Jair/jair.js
+++ Jair/jair.js
@@ -6,12 +6,12 @@ function mostrarFormulario()
     var form = document.createElement('form');
     form.id = 'formulario-datos';
     var nombreDiv = document.createElement('div');
-    nombreDiv.innerHTML = '<label for="nombre">Nombre:</label><input type="text" id="nombre" name="nombre" required>';
+    nombreDiv.innerHTML = '<label for="nombre">Nombre: </label><input type="text" id="nombre" name="nombre" required>';
     var emailDiv = document.createElement('div');
-    emailDiv.innerHTML = '<label for="email">Email:</label><input type="email" id="email" name="email" required>';
+    emailDiv.innerHTML = '<label for="email">Email: </label><input type="email" id="email" name="email" required>';
 
     var mensajeDiv = document.createElement('div');
-    mensajeDiv.innerHTML = '<label for="mensaje">Mensaje:</label><textarea id="mensaje" name="mensaje" rows="4" required></textarea>';
+    mensajeDiv.innerHTML = '<label for="mensaje">Mensaje: </label><textarea id="mensaje" name="mensaje" rows="4" required></textarea>';
 
     var btn_enviar = document.createElement('button');
     btn_enviar.type = 'submit';
